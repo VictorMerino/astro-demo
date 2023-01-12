5dda2edb5d08a026af58546bc508e12df7263182
Víctor Merino <victor.merino@jungheinrich.es>
2023-01-12 13:01:06 +0100

    Add config to deploy in GH-Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://VictorMerino.github.io',
+  base: '/astro-course',
   integrations: [react(), tailwind()]
 });
