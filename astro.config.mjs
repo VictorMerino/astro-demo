f0c5611440dc4be7a2277189aef2cdd84015a282
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2023-01-15 13:52:21 +0100

    Vue Example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://VictorMerino.github.io',
   base: '/astro-demo',
-  integrations: [react(), tailwind()],
-})
+  integrations: [react(), tailwind(), vue()]
+});
---
+++ src/components/examples/ButtonsInVue.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import { ref } from 'vue';
+
+  const counter = ref(0)
+  function count() {
+    counter.value++
+  }
+</script>
+<template>
+  <button
+    class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow m-4"
+    @click="count"
+  >
+    Click to count {{counter}}
+  </button>
+</template>
